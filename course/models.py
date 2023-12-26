c0251bb638e60650563bec5de80d3e427d5b892c
Mostafa <mostafa.saneisn@gmail.com>
2023-12-26 20:37:05 +0330

    add: comments on course models

--- course/models.py
+++ course/models.py
@@ -3,9 +3,19 @@ from django.db.models import CharField
 
 
 class Teacher(models.Model):
+    '''
+    args: name and about.
+
+    returns: a teacher model in admin panel
+    '''
     name = models.CharField(max_length=50)
     about = models.TextField()
 
+    '''
+    returns:
+    a list for courses that a teacher has.
+    (is not working!)
+    '''
     @property
     def courses():
         return list[Course]
@@ -14,6 +24,13 @@ class Teacher(models.Model):
         return f'{self.name}'
 
 class Main_Learn(models.Model):
+    '''
+    args:name, about
+
+
+    returns: the main category of courses.
+    (already defined!)
+    '''    
     name = models.CharField(max_length=50,  choices=[('برنامه‌نویسی', 'برنامه‌نویسی'),
                                                      ('زبان‌های خارجی', 'زبان‌های خارجی'),
                                                      ('آی‌تی و نرم‌افزار', 'آی‌تی و نرم‌افزار'),
@@ -28,8 +45,12 @@ class Main_Learn(models.Model):
     def __str__(self) -> str:
         return f'{self.name}'
 
-
 class Sub_Learn(models.Model):
+    '''
+    args: name, about, subjects(from the Main_learn) 
+
+    returns: the second level of category.
+    '''
     name = models.CharField(max_length=50)
     about = models.TextField()
     subjects = models.ForeignKey(Main_Learn, on_delete=models.CASCADE)
@@ -38,6 +59,11 @@ class Sub_Learn(models.Model):
         return f'{self.name}'
     
 class Sub_Sub_Learn(models.Model):
+    '''
+    args:name, subject(from Sub_sub_learn)
+
+    returns:this is the 3rd and alst  level of category.
+    '''
     name = models.CharField(max_length=50)
     subjects = models.ForeignKey(Sub_Learn, on_delete=models.CASCADE)
 
@@ -45,6 +71,18 @@ class Sub_Sub_Learn(models.Model):
         return f'{self.name}'
 
 class Course(models.Model):
+    '''
+    args: 
+    name,
+    number(for identification of objects),
+    teacher(from Teacher model),
+    category(from Sub_sub_learn),
+    description, price, about,
+    other courses needed(this is inherited from itself the Course model)
+
+    returns: 
+    the course objects that we want to display in website.
+    '''
     name = models.CharField(max_length=255)
     number = models.IntegerField(unique= True)
     teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
@@ -58,18 +96,32 @@ class Course(models.Model):
         return f'{self.name}'
 
 class Ticket(models.Model):
+    '''
+    args:
+    the arguments are provided by user and are posted to this model.
+
+    returns:
+    the information of the purchase by user.
+    '''
     course = models.ForeignKey(Course, on_delete=models.PROTECT)
     name = models.CharField(max_length= 200)
     last_name = models.CharField(max_length= 200)
     email = models.EmailField()
     phonenumber = models.CharField(max_length=11)
-    # seat = models.PositiveIntegerField()
     reservation = models.CharField(max_length= 8, unique = True)
     
     def __str__(self) -> str:
         return f'{self.name} {self.last_name}'
     
 def teacher_courses(Teacher):
+    '''
+    args:
+    takes the info of one teacher.
+
+    returns:
+    the list of courses that teacher has.
+    (does not work!)
+    '''
     for i in Course.teacher:
         if Teacher == i:
             return  Course.name
